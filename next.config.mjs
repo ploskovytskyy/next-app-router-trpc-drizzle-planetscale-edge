29c7e56a14d936627eb7fcb4089e4688493b71a2
Oleksandr Ploskovytskyy <oleksandr.ploskovytskyy@nfq.pl>
2023-04-15 09:50:47 +0200

    init

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  experimental: { appDir: true, typedRoutes: true },
+};
+export default config;
