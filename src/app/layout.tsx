ee5d79f442f6291681a3f6fabff9e59c5a42b1b5
Oleksandr Ploskovytskyy <oleksandr.ploskovytskyy@nfq.pl>
2023-04-15 10:32:22 +0200

    app dir, site config

---
+++ src/app/layout.tsx
@@ -0,0 +1,84 @@
+import { Inter } from "next/font/google";
+
+import "~/styles/globals.css";
+import { siteConfig } from "~/config/site";
+import { absoluteUrl, cn } from "~/lib/utils";
+
+const fontSans = Inter({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "App-Router",
+    "TRPC",
+    "Edge",
+    "Drizzle",
+    "PlanetScale",
+    "T3",
+    "Stack",
+    "Tailwind",
+    "shadcn/ui",
+    "Radix",
+  ],
+  authors: [
+    {
+      name: "Oleksandr Ploskovytskyy",
+      url: "",
+    },
+  ],
+  creator: "Oleksandr Ploskovytskyy",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: absoluteUrl("/og.jpg"),
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@o_ploskovytskyy",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
+type RootLayoutProps = PropsWithChildren;
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <head />
+      <body className={cn("min-h-screen antialiased", fontSans.className)}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,20 @@
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+  };
+};
+
+export const siteConfig: SiteConfig = {
+  name: "Next App Router with TRPC and Drizzle on Edge",
+  description:
+    "T3 stack using app router with TRPC on edge, Drizzle Orm with PlanetScale serverless driver",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "https://twitter.com/o_ploskovytskyy",
+  },
+};
